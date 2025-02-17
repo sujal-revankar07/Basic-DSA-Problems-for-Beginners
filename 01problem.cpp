9f886a9aa695ac87e47de093d9c3488ebd9096eb
sujal <sujalrevankar765@gmail.com>
2025-02-18 00:13:58 +0530

    Initial commit

---
+++ 01problem.cpp
@@ -0,0 +1,29 @@
+// Question 01:
+// Write a program that takes an integer n as input and prints a pattern where each row contains numbers from 1 to n and repeats for n rows.
+// Example:
+// Input:
+// 4
+
+// Output:
+// 1234  
+// 1234  
+// 1234  
+// 1234  
+
+// CODE -->
+
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "n :";
+    cin  >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1 ; j <=n; j++)
+        {
+            cout << j;
+        }
+        cout << "\n";
+    }
+}
