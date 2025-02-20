0fefea0f562e466b45602b67126ac85114dfc699
sujal <sujalrevankar765@gmail.com>
2025-02-20 23:50:52 +0530

    Initial commit

--- Patterns/03problem.cpp
+++ Patterns/03problem.cpp
@@ -1,3 +1,21 @@
+// Write a program that takes an integer n as input and prints an n × n grid of sequential numbers starting from 1, increasing from left to right, and continuing to the next row.
+
+// Example:
+// Input:
+// n : 4
+
+
+// Output:
+//   1  2  3  4  
+//   5  6  7  8  
+//   9 10 11 12  
+//  13 14 15 16  
+
+
+
+
+
+
 #include<iostream>
 using namespace std;
 int main() {
